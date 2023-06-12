2f7112b7834801c3ac56ba68e86a7ef14f91ae25
arysttoto <aryst.toto@gmail.com>
2023-06-12 15:41:54 +0600

    writings for dark/light modes added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,9 +15,11 @@ function Header () {
     const toggleTheme = (isLight) => {          
       if (isLight) {
         document.documentElement.classList.remove("dark");  
+        document.querySelector(".toggle-theme-title").innerHTML = "Dark mode!"; 
       }
       else {
         document.documentElement.classList.add("dark");  
+        document.querySelector(".toggle-theme-title").innerHTML = "Light mode!"; 
       }   
     }
 
@@ -39,8 +41,8 @@ function Header () {
                     <Button class="relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
                       function={changeTheme} 
                     >
-                      <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                          Change theme
+                      <span className="toggle-theme-title relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                          Dark mode!
                       </span>
                     </ Button> 
                     {/* <button
